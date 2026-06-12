c31448b0b8e360187cd27c0bfeab1ac106a3c465
steve007redog-blip <steve007redog@gmail.com>
2026-06-12 17:37:15 +1200

    Add files via upload

---
+++ js/suppliers.js
@@ -0,0 +1,10 @@
+const SUPPLIERS = {
+    "DIC": ["DICNZ.CustomerServices@dic.co.nz"],
+    "DKSH": ["rick.menalda@dksh.com", "Sales.pM.NZ@dksh.com"],
+    "Miscellaneous": ["oggiowens@outlook.com"]
+};
+
+const CC_RECIPIENTS = [
+    "iris@lamprint.co.nz",
+    "todd@lamprint.co.nz"
+];
